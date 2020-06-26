16c90c98a175584f1ca9b1e2785665d22caa75a6
Jdekock1184 <Jdekock1184@gmail.com>
2020-06-26 14:56:30 +0200

    Chat Box

---
+++ Client/wwwroot/Reg.js
@@ -0,0 +1,40 @@
+﻿
+
+function setItem(name, surname, username)
+{
+    window.sessionStorage["Name"] = name;
+    window.sessionStorage["Surname"] = surname;
+    window.sessionStorage["Username"] = username;
+}
+
+
+function getItem(Username)
+{
+    return window.sessionStorage[Username];
+}
+
+/*
+function setItem2(Surname, value)
+    {
+        window.sessionStorage[Surname] = value;
+    }
+
+function setItem3(Username, value)
+    {
+        window.sessionStorage[Username] = value;
+    }
+
+function getItemname(Name)
+    {
+        return window.sessionStorage[Name];
+    }
+
+function getItemsurname(Surname)
+    {
+        return window.sessionStorage[Surname];
+    }
+
+function getItemusername(Username) {
+        return window.sessionStorage[Username];
+    }
+*/
